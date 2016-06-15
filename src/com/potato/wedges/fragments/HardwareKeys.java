52151b1463dfd4d528b76bbb0a65692a4b47fa6c
GodfatherX64 <godfather.x64@gmail.com>
2016-06-15 12:51:05 +0200

    Enable single tap rebinding for hwkey back and home
    Initially, this policy was put in place to safeguard
    the UX by preventing a usecase where the device could
    be put in a state without back or home available. However,
    if a user puts the device in this state, it is easy enough
    to navigate to button settings to fix it
    
    Change-Id: I6b71f9e54fcb99356e07f236e9ed926ac5fc017e
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- src/com/potato/wedges/fragments/HardwareKeys.java
+++ src/com/potato/wedges/fragments/HardwareKeys.java
@@ -113,6 +113,11 @@ public class HardwareKeys extends ActionFragment implements Preference.OnPrefere
     }
 
     @Override
+    protected boolean usesExtendedActionsList() {
+        return true;
+    }
+
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         return false;
     }
