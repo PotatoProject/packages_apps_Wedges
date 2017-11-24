107feadfd4c3179ef02f3ca235409ef64644c47a
bigrushdog <randall.rushing@gmail.com>
2017-11-23 23:07:52 -0500

    DUI: Initial checkin for Oreo [4/7]
    Change-Id: I341fda5e7c3e65468bdde2e4cced696562b2756c
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- src/com/potato/wedges/preferences/SecureSettingSwitchPreference.java
+++ src/com/potato/wedges/preferences/SecureSettingSwitchPreference.java
@@ -55,11 +55,4 @@ public class SecureSettingSwitchPreference extends SwitchPreference {
         return Settings.Secure.getInt(getContext().getContentResolver(),
                 getKey(), defaultReturnValue ? 1 : 0) != 0;
     }
-
-    @Override
-    protected boolean isPersisted() {
-        // Using getString instead of getInt so we can simply check for null
-        // instead of catching an exception. (All values are stored as strings.)
-        return Settings.Secure.getString(getContext().getContentResolver(), getKey()) != null;
-    }
 }
