35ee3638eaa8d269547e17c9f225e51026d79822
Kshitij Gupta <kshitijgm@gmail.com>
2018-07-10 20:13:57 +0530

    EdgeGestures: Redo navbar toggling and cleanup
    Change-Id: Ifd229cf83bdc9e1293f296c4051b0df9c9ba280b
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- src/com/potato/wedges/fragments/EdgeGesturesSettings.java
+++ src/com/potato/wedges/fragments/EdgeGesturesSettings.java
@@ -8,7 +8,6 @@ import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference;
 
 import com.android.internal.logging.nano.MetricsProto;
-import com.android.internal.utils.du.DUActionUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.potato.wedges.preferences.SecureSettingSeekBarPreference;
@@ -31,8 +30,6 @@ public class EdgeGesturesSettings extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.edge_gestures);
 
         enabledPreference = (SwitchPreference) findPreference(EDGE_GESTURES_ENABLED);
-        /*enabledPreference.setChecked((Settings.System.getInt(getContentResolver(),
-                Settings.Secure.EDGE_GESTURES_ENABLED, 0) == 1));*/
         enabledPreference.setOnPreferenceChangeListener(this);
 
         screenPercentPreference = (SecureSettingSeekBarPreference) findPreference(EDGE_GESTURES_SCREEN_PERCENT);
@@ -67,30 +64,15 @@ public class EdgeGesturesSettings extends SettingsPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (preference == enabledPreference) {
             int enabled = ((boolean) newValue) ? 1 : 0;
-            //Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_ENABLED, enabled, UserHandle.USER_CURRENT);
-
-            if (enabled == 1) {
+            if ((boolean) newValue) {
                 Settings.Secure.putInt(getContentResolver(),
-                        Settings.Secure.NAVIGATION_BAR_VISIBLE,
-                        0);
+                    Settings.System.NAVIGATION_BAR_ENABLED, 0);
             } else {
-                if (DUActionUtils.hasNavbarByDefault(getPrefContext())) {
-                    Settings.Secure.putInt(getContentResolver(),
-                            Settings.Secure.NAVIGATION_BAR_VISIBLE,
-                            1);
-                }
+                Settings.Secure.putInt(getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_ENABLED, 1);
             }
             return true;
-        } /*else if (preference == hapticFeedbackDurationPreference) {
-            int hapticFeedbackValue = Integer.valueOf((String) newValue);
-            Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_FEEDBACK_DURATION, hapticFeedbackValue, UserHandle.USER_CURRENT);
-            return true;
-        } else if (preference == longPressDurationPreference) {
-            int longPressValue = Integer.valueOf((String) newValue);
-            Settings.Secure.putIntForUser(getContentResolver(), Settings.Secure.EDGE_GESTURES_LONG_PRESS_DURATION, longPressValue, UserHandle.USER_CURRENT);
-            return true;
-        }*/
-
+        }
         return false;
     }
 }
